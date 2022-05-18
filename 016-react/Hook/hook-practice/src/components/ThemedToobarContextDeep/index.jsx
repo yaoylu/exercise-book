6a6916cfabd4c0f06dfca56a1f2deb33c537e202
yaoylu <alysa.yy.lu@gmail.com>
2022-05-18 20:08:18 +0800

    fix(react):comtext传递通过state中的一个obj

--- 016-react/Hook/hook-practice/src/components/ThemedToobarContextDeep/index.jsx
+++ 016-react/Hook/hook-practice/src/components/ThemedToobarContextDeep/index.jsx
@@ -7,26 +7,30 @@ export class ThemedToolbarContextDeep extends React.Component {
     super(props);
 
     this.toggleTheme = () => {
-      this.setState(state => ({
-        theme:
-          state.theme === themes.dark
-            ? themes.light
-            : themes.dark,
-      }));
+      const { themeObj } = this.state;
+      themeObj.theme =  this.state.themeObj.theme === themes.dark
+        ? themes.light
+        : themes.dark;
+      this.setState({
+        themeObj,
+      });
     };
 
     // State 也包含了更新函数，因此它会被传递进 context provider。
     this.state = {
-      theme: themes.light,
-      toggleTheme: this.toggleTheme,
+      themeObj: {
+        theme: themes.light,
+        toggleTheme: this.toggleTheme,
+      },
+
     };
   }
 
   render() {
     // 整个 state 都被传递进 provider
-    const { theme, toggleTheme } = this.state;
+    // const { theme, toggleTheme } = this.state;
     return (
-      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <ThemeContext.Provider value={this.state.themeObj}>
         <Content />
       </ThemeContext.Provider>
     );
